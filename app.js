850de357c30becd2c33af37ae907afb791b4cf44
Aman Anurag <amaxanurag.20@gmail.com>
2023-10-31 07:57:35 +0530

    episode1

---
+++ app.js
@@ -0,0 +1,11 @@
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"i am h1 tag"),
+    React.createElement("h2",{},"i am h2 tag"),
+]))
+console.log(parent);
+// const heading =React.createElement("h1",{id:"game",abc:"xyz"},"Namaste React..");
+
+
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);  
